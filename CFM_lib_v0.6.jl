ad3b4e113274167684f1a90ff561645d9ee5a302
karunru <1qazhikaru@gmail.com>
2019-01-22 13:18:54 +0900

    julia 1.0 に対応

--- CFM_lib_v0.6.jl
+++ CFM_lib_v0.6.jl
@@ -1,7 +1,4 @@
 using IterativeSolvers
-using SparseArrays
-using Arpack
-using Statistics
 
 """ Convex Factorization Machine with Hazan's algorithm (squared loss)
     Yamada, M. et. al., http://arxiv.org/abs/1507.01073
@@ -27,7 +24,16 @@ U: double
    d by T dimensional matrix such that W = U U^t
 
 """
-function train(iter, reg, X, Y)
+mutable struct CFM
+  num_iter::Int64
+  reg_W::Float64
+  w::Array{Float64, 1}
+  U::Array{Float64, 2}
+
+  CFM(num_iter, reg_W) = new(num_iter, reg_W, Float64[], Array{Float64}(0,0))
+end
+
+function train(model::CFM, X, Y)
   X = convert(SparseMatrixCSC{Float64,Int64}, X)
   Y = Array(Y)
 
@@ -36,30 +42,30 @@ function train(iter, reg, X, Y)
   #Add bias for training data
   Z = hcat(ones(n), X)
 
-  T = iter
-  η = reg
+  T = model.num_iter
+  η = model.reg_W
 
-  U = zeros(d, T)
+  model.U = zeros(d, T)
   P = zeros(d, T)
   λ = ones(T)
   w = zeros(d+1, 1)
   global fval = zeros(T)
 
   for t in 1:T
-    tmp = X * U[:, 1:t]
-    fQ = 0.5 * (vec(sum(tmp .* tmp, dims=2)) - (X .* X) * vec(sum(U[:, 1:t] .* U[:, 1:t], dims=2)))
-    
+    tmp = X * model.U[:, 1:t]
+    fQ = 0.5 * (vec(sum(tmp .* tmp, 2)) - (X .* X) * vec(sum(model.U[:, 1:t] .* model.U[:, 1:t], 2)))
+
     ZY = Z' * (Y - fQ)
 
     #Conjugate Gradient: Solve Zw = Y
     wout = cg!(w, Z'*Z, ZY, tol=1e-6, verbose = false, maxiter=100)
-    w = vec(wout)
+    model.w = vec(wout)
 
-    tr_err = Y - (Z*w + fQ)
+    tr_err = Y - (Z*model.w + fQ)
 
     #Frank-Wolfe update: eigs(X diag(tr_err) X^t, 1)
-    pout = eigs(X'*spdiagm(0 => sparsevec(tr_err))*X, nev=1, which=:LR, maxiter = 300, tol = 1e-1)[2]
-        
+    pout = eigs(X'*spdiagm(sparsevec(tr_err))*X, nev=1, which=:LR, maxiter = 300, tol = 1e-1)[2]
+
     p = vec(real(pout))
 
     #Optimal step size
@@ -71,7 +77,7 @@ function train(iter, reg, X, Y)
     λ[1:t-1] = (1 - α) * λ[1:t-1]
     λ[t] = max(1e-10, α)
 
-    U[:, 1:t] = P[:, 1:t] .* sqrt.(λ[1:t])'
+    model.U[:, 1:t] = P[:, 1:t] .* sqrt.(λ[1:t])'
 
     #Traning RMSE
     global fval[t] = sqrt(mean(tr_err.^2))
@@ -79,28 +85,26 @@ function train(iter, reg, X, Y)
     #fval: training RMSE
     println("iteration:", t, " Training RMSE:", fval[t])
 
-    tmp = nothing
-    fQ = nothing
-    ZY = nothing
-    wout = nothing
-    tr_err = nothing
-    pout = nothing
-    p = nothing
-    err = nothing
-    α= nothing
-    end
-    
-    return w, U
+		tmp = nothing
+		fQ = nothing
+		ZY = nothing
+		wout = nothing
+		tr_err = nothing
+		pout = nothing
+		p = nothing
+		err = nothing
+		α= nothing
+	end
 end
 
-function predicter(w, U, X)
-    X = convert(SparseMatrixCSC{Float64,Int64}, X)
+function predicter(model::CFM, X)
+  X = convert(SparseMatrixCSC{Float64,Int64}, X)
 
-    #Compute fQ (2-way factor)
-    tmp = X * U
-    fQ = 0.5 * (vec(sum(tmp .* tmp, dims=2)) - (X .* X) * vec(sum(U .* U, dims=2)))
+  #Compute fQ (2-way factor)
+  tmp = X * model.U
+  fQ = 0.5 * (sum(tmp .* tmp, 2) - (X .* X) * sum(model.U .* model.U, 2))
 
-    Ŷ = w[1] .+ X * w[2:end] + fQ
+  Ŷ = model.w[1] + X * model.w[2:end] + fQ
 
   return Ŷ
 end
